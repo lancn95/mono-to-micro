82d3e5a673aaf49858e36fa4f594889e0fb22a51
Kevin Cat Ngoc Lan <catngoclan95@gmail.com>
2024-09-22 21:16:36 +0700

    feat edit dep and ser

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3e44b40847f04a3fb936481f55cad9a-591085407.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
